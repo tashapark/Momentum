ae6eb4a319cc4cad73f9b623144cb197080d78cf
TashaPark <pdy2564@naver.com>
2024-02-21 21:17:19 +0900

    calculator

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("hi");
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  powerOf: function (a, b) {
+    console.log(a ** b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+};
+
+calculator.plus(1, 1);
+calculator.minus(1, 1);
+calculator.powerOf(3, 3);
+calculator.divide(80, 4);
