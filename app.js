2089303026385dbc533561f479282b33bccd0741
TashaPark <pdy2564@naver.com>
2024-02-21 22:14:56 +0900

    better using return

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const calculator = {
   plus: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
   },
   powerOf: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
+  },
+  multiply: function (a, b) {
+    return a * b;
   },
 };
-
-calculator.plus(1, 1);
-calculator.minus(1, 1);
-calculator.powerOf(3, 3);
-calculator.divide(80, 4);
+//return을 쓰면 콘솔 창이 아닌 화면에 나타나게 할 수 있고, 앞에 계산된 거 가져다가 할 수도 있음.
+const plusResult = calculator.plus(8, 1); // console.log 하고 싶으면 변수 명을 입력하면 됨.
+const minusResult = calculator.minus(plusResult, 1);
+const multiplyResult = calculator.multiply(10, minusResult);
+const divideResult = calculator.divide(multiplyResult, plusResult);
+const powerResult = calculator.powerOf(divideResult, minusResult);
