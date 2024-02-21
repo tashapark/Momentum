3afbb6d71cfe73dc41938397e74a9c1c776b02c3
TashaPark <pdy2564@naver.com>
2024-02-22 01:05:53 +0900

    condition practice

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  powerOf: function (a, b) {
-    return a ** b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  multiply: function (a, b) {
-    return a * b;
-  },
-};
-//return을 쓰면 콘솔 창이 아닌 화면에 나타나게 할 수 있고, 앞에 계산된 거 가져다가 할 수도 있음.
-const plusResult = calculator.plus(8, 1); // console.log 하고 싶으면 변수 명을 입력하면 됨.
-const minusResult = calculator.minus(plusResult, 1);
-const multiplyResult = calculator.multiply(10, minusResult);
-const divideResult = calculator.divide(multiplyResult, plusResult);
-const powerResult = calculator.powerOf(divideResult, minusResult);
+const age = parseInt(prompt("How old are you?"));
+
+if (isNaN(age) || age < 0) {
+  //=== true
+  console.log("please write a real positive number");
+} else if (age < 18) {
+  console.log("you are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("you can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("you should exercise");
+} else if (age === 100) {
+  //순서가 중요하니까 잘 생각할 것. 이게 아래면,절대 안 나타남.
+  console.log("wow!");
+} else {
+  console.log("you can do whatever you want");
+}
