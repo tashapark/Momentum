5bb8abf7e9827fe3cc0ef30b3af24e5969404ed7
TashaPark <pdy2564@naver.com>
2024-02-27 20:50:21 +0900

    using hidden class for form

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+//2번 반복되니깐, 변수 해줌. 대문자로 쓰는 것은 string으로만 저장하고 싶으르 때 사용.
+//즉 중요 정보는 아님
 
 function onLoginSubmit(event) {
-  // argument들어감. 첫번째 인자는 앞으로 벌어질 일들에 대한 정보를 제공.
-  // 공간만 있으면 걍 알아서 함수 진행함.
-  // 아무것도 안 넣어도 되지만, 넣으면 js가 알아서 진행해 주니깐, 주로 event를 넣음.
   event.preventDefault();
-  console.log(loginInput.value); // 새로고침 안되고 출력됨.
+  loginForm.classList.add(HIDDEN_CLASSNAME); // 폼이 사라지게 클래스 .hidden이 나타나도록
+
+  const username = loginInput.value; //입력 값 저장
+  greeting.innerText = `hello ${username}`; // h1 값을 입력 내용으로 변경
+  greeting.classList.remove(HIDDEN_CLASSNAME); // hidden을 보이게 없애줌.
 }
+
 loginForm.addEventListener("submit", onLoginSubmit);
-// submit은 엔터치거나, 버튼 누를 때 발생
-//submit 디폴트가 새로고침임. so, 못하게 해야 함.
-//onLoginSubmit에 () 안 붙여도 js가 위의 알규먼트를 보고 알아서 실행 해줌.
+// 첫번째 인자만 넣어주면 알아서 js가 계산함.
+// ()넣지 말 것. 넣으면 직접 실행한다는 건데 그게 아니라 js가 하도록
