51e4662e5bf31ddb2eb0bc7158de77f284398572
TashaPark <pdy2564@naver.com>
2024-02-27 18:23:45 +0900

    input inside the form

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function handleLoginBtnClick() {
-  const username = loginInput.value;
-  if (username === "") {
-    alert("please write your name");
-  } else if (username.length > 15) {
-    alert("Your name is too long.");
-  }
+  console.log(username);
 }
 
 // username이 비어있거나, 너무 길면 x
