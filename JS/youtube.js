796a3219bf46f5fdba087e8c225e941bc1c0efe4
TashaPark <pdy2564@naver.com>
2024-03-14 00:02:31 +0900

    add youtube api

--- JS/youtube.js
+++ JS/youtube.js
@@ -3,7 +3,7 @@
 // 2. 이 코드는 Iframe Player API를 비동기적으로 로드한다. !!필수!!
 const tag = document.createElement("script");
 
-tag.src = "http://www.youtube.com/iframe_api";
+tag.src = "https://www.youtube.com/iframe_api";
 const firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
@@ -11,9 +11,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 let player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
-    height: "180", //변경가능-영상 높이
-    width: "320", //변경가능-영상 너비
-    videoId: "FxB6_qaqHlY", //변경-영상ID //이게 지금.. 밑에 플레이 리스트랑 다르면 리스트 영상으로됨
+    videoId: "s56deFmiIh0", //변경-영상ID //산나비.
     playerVars: {
       rel: 1, //연관동영상 표시여부(0:표시안함)
       controls: 1, //플레이어 컨트롤러 표시여부(0:표시안함)
@@ -21,11 +19,17 @@ function onYouTubeIframeAPIReady() {
       mute: 1, //음소거여부(1:음소거 함)
       loop: 1, //반복재생여부(1:반복재생 함)
       playsinline: 1, //iOS환경에서 전체화면으로 재생하지 않게
-      playlist: "W0IPdHFSgX8", //재생할 영상 리스트 // 추가하는 법을 모르겠고..
-    },
-    events: {
-      onReady: onPlayerReady, //onReady 상태일 때 작동하는 function이름
-      onStateChange: onPlayerStateChange, //onStateChange 상태일 때 작동하는 function이름
+      playlist:
+        "AKWlyl8_sdM,4Hm56tqcOc,s56deFmiIh0,PfqRyzUuHnM,gOjkGLBZhZo,4YUuA-hPDZA,FDI8d7jm4zU,BJ2U4t1jruA,wkFL8Oa0p30,o3ORqZYUccE,POsucxJPxqM,VFosjkiy7bw",
+      //이러다 공허하게 //내가 죽으려고 //산나비 //말포이 공부중 //내 비평가들 //바닷가 카페 //나비 보베따
+      //바람이 물었다 //벅찬 날//쇼팽 왈츠//웬즈데이 //포기할 이유가 없다 //시절인연
+      shuffle: 1,
+      //재생할 영상 리스트 //
+
+      events: {
+        onReady: onPlayerReady, //onReady 상태일 때 작동하는 function이름
+        onStateChange: onPlayerStateChange, //onStateChange 상태일 때 작동하는 function이름
+      },
     },
   });
 }
@@ -38,6 +42,7 @@ function onPlayerReady(event) {
 // 5. API는 플레이어의 상태가 변화될 때 아래의 function을 불러올 것이다.
 //    이 function은 비디오가 재생되고 있을 때를 가르킨다.(state=1),
 //    플레이어는 6초 이상 재생되고 정지되어야 한다.
+// 플레이어 상태가 변경 될 때 실행
 var done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
@@ -45,6 +50,7 @@ function onPlayerStateChange(event) {
     done = true;
   }
 }
+//6. 재생 정지 함수
 function stopVideo() {
   player.stopVideo();
 }
