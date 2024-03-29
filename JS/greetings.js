2f629a9fae064bb0a27d7e42559f4721ed29510b
TashaPark <pdy2564@naver.com>
2024-03-29 18:09:15 +0900

    :bug::memo:rename

--- JS/greetings.js
+++ JS/greetings.js
@@ -7,6 +7,8 @@ const USERNAME_KEY = "username";
 //2번 반복되니깐, 변수 해줌. 대문자로 쓰는 것은 string으로만 저장하고 싶으르 때 사용.
 //즉 중요 정보는 아님. 반복 쓸 떄 오타를 줄이기 위해서 변수로 하면, 에러 확인 가능
 
+let savedUsername = localStorage.getItem(USERNAME_KEY);
+
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME); // 폼이 사라지게 클래스 .hidden이 나타나도록
@@ -15,13 +17,22 @@ function onLoginSubmit(event) {
   paintGreetings(username); // h1 값을 입력 내용으로 변경 // hidden을 보이게 없애줌.
 }
 
+function onRenameToggle() {
+  loginForm.classList.toggle(HIDDEN_CLASSNAME);
+  greeting.classList.toggle(HIDDEN_CLASSNAME);
+}
+//이름 수정하는 버튼
+const buttonRename = document.createElement("button");
+buttonRename.classList.add("buttonRename");
+buttonRename.innerText = "✍️";
+buttonRename.addEventListener("click", onRenameToggle);
+document.body.appendChild(buttonRename);
+
 function paintGreetings(username) {
   // 위 아래 반복되서 함수로 만들어서 뺐음
   greeting.innerText = `Hello ${username} :)`; //먼저 그리팅 넣고
   greeting.classList.remove(HIDDEN_CLASSNAME); // 히든 없애기.. 보이게
 }
-
-const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   // LS 에 없으면 form 보여주기
