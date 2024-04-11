dafab04e66fb5c366ed88db85a7aee65fe53a9f0
TashaPark <pdy2564@naver.com>
2024-04-11 18:56:40 +0900

    :recycle:

--- JS/youtube.js
+++ JS/youtube.js
@@ -74,7 +74,7 @@ function onYouTubeIframeAPIReady() {
     playerVars: {
       rel: 1, //연관동영상 표시여부(0:표시안함)
       controls: 1, //플레이어 컨트롤러 표시여부(0:표시안함)
-      autoplay: 1, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
+      autoplay: 0, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
       mute: 1, //음소거여부(1:음소거 함)
       loop: 1, //반복재생여부(1:반복재생 함)
       playsinline: 1, //iOS환경에서 전체화면으로 재생하지 않게
@@ -102,7 +102,7 @@ function onYouTubeIframeAPIReady() {
     playerVars: {
       rel: 1, //연관동영상 표시여부(0:표시안함)
       controls: 1, //플레이어 컨트롤러 표시여부(0:표시안함)
-      autoplay: 1, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
+      autoplay: 0, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
       mute: 1, //음소거여부(1:음소거 함)
       loop: 1, //반복재생여부(1:반복재생 함)
       playsinline: 1, //iOS환경에서 전체화면으로 재생하지 않게
@@ -126,7 +126,7 @@ function onYouTubeIframeAPIReady() {
     playerVars: {
       rel: 1, //연관동영상 표시여부(0:표시안함)
       controls: 1, //플레이어 컨트롤러 표시여부(0:표시안함)
-      autoplay: 1, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
+      autoplay: 0, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
       mute: 1, //음소거여부(1:음소거 함)
       loop: 1, //반복재생여부(1:반복재생 함)
       playsinline: 1, //iOS환경에서 전체화면으로 재생하지 않게
@@ -150,7 +150,7 @@ function onYouTubeIframeAPIReady() {
     playerVars: {
       rel: 1, //연관동영상 표시여부(0:표시안함)
       controls: 1, //플레이어 컨트롤러 표시여부(0:표시안함)
-      autoplay: 1, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
+      autoplay: 0, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
       mute: 1, //음소거여부(1:음소거 함)
       loop: 1, //반복재생여부(1:반복재생 함)
       playsinline: 1, //iOS환경에서 전체화면으로 재생하지 않게
@@ -178,7 +178,7 @@ function onYouTubeIframeAPIReady() {
       playerVars: {
         rel: 1, //연관동영상 표시여부(0:표시안함)
         controls: 1, //플레이어 컨트롤러 표시여부(0:표시안함)
-        autoplay: 1, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
+        autoplay: 0, //자동재생 여부(1:자동재생 함, mute와 함께 설정)
         mute: 1, //음소거여부(1:음소거 함)
         loop: 1, //반복재생여부(1:반복재생 함)
         playsinline: 1, //iOS환경에서 전체화면으로 재생하지 않게
