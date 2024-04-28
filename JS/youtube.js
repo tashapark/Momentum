c5730d07e68fa41d060130b911bee5f5f94e2577
TashaPark <pdy2564@naver.com>
2024-04-28 23:43:16 +0900

    :memo:change first video

--- JS/youtube.js
+++ JS/youtube.js
@@ -101,7 +101,7 @@ function onYouTubeIframeAPIReady() {
   player1.getIframe().classList.add(HIDDEN_TOGGLE);
 
   player2 = new YT.Player("player2", {
-    videoId: "SPTgBCGBVQk", //변경-영상ID //lady
+    videoId: "zStA6usjJYQ", //변경-영상ID //준더그라운드
     playerVars: {
       rel: 1, //연관동영상 표시여부(0:표시안함)
       controls: 1, //플레이어 컨트롤러 표시여부(0:표시안함)
